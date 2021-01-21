9f67b7276eeb0f4cad14d3615e94fc8f9cb3fb22
jiwon-ryu <jiwon.ryu@snu.ac.kr>
2021-01-21 17:35:06 +0900

    new

--- baseball_game.py
+++ baseball_game.py
@@ -7,7 +7,7 @@ def get_random_number():
     return random.randrange(100, 1000)
 
 
-def is_digit(user_input_number):
+def is_digit(user_input_number: str):
     try:
         int(user_input_number)
         return True
@@ -15,7 +15,7 @@ def is_digit(user_input_number):
         return False
 
 
-def is_between_100_and_999(user_input_number):
+def is_between_100_and_999(user_input_number: str):
     if int(user_input_number) >= 100 and int(user_input_number) < 1000:
         return True
     else:
@@ -23,14 +23,14 @@ def is_between_100_and_999(user_input_number):
         
 
 
-def is_duplicated_number(three_digit):
+def is_duplicated_number(three_digit: str):
     if len(list(set(list(three_digit)))) == len(list(three_digit)):
         return False
     else:
         return True
 
 
-def is_validated_number(user_input_number):
+def is_validated_number(user_input_number: str):
     if is_digit(user_input_number) == True:
         if is_between_100_and_999(user_input_number) == True:
             if is_duplicated_number(user_input_number) == False:
@@ -42,10 +42,10 @@ def get_not_duplicated_three_digit_number():
     random_number_result = get_random_number()
     while is_duplicated_number(str(random_number_result)) == True:
         random_number_result = get_random_number()
-    return random_number_result
+    return random_number_result # int
 
 
-def get_strikes_or_ball(user_input_number, random_number):
+def get_strikes_or_ball(user_input_number: str, random_number: str):
     result = [0, 0] # strikes, balls
     for digit in user_input_number:
         if digit in random_number:
@@ -56,7 +56,7 @@ def get_strikes_or_ball(user_input_number, random_number):
     return result
 
 
-def is_yes(one_more_input):
+def is_yes(one_more_input: str): # in으로 고쳐도 될듯
     if one_more_input.upper() == 'Y' or one_more_input.upper() == 'YES':
         return True
     else:
@@ -90,10 +90,10 @@ def main():
         print(f'Strikes : {strikes} , Balls : {balls}')
 
     if strikes == 3:
-        response = input('You win, one more(Y/N)?')
+        response = input('You win, one more(Y/N)? ')
         while is_yes(response) == False and is_no(response) == False:
             print('Wrong Input')
-            response = input('You win, one more(Y/N)?')
+            response = input('You win, one more(Y/N)? ')
         if is_yes(response) == True:
             main()
         else:
